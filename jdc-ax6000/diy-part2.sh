c9c3bb09401e70d9f82f7f67a49055ff42b0a9b0
he-yu-qing <70266527+he-yu-qing@users.noreply.github.com>
2025-12-03 10:17:30 +0800

    Update diy-part2.sh

--- jdc-ax6000/diy-part2.sh
+++ jdc-ax6000/diy-part2.sh
@@ -23,3 +23,57 @@ rm -rf feeds/packages/net/open-app-filter
 # rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------
 # sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+cat >> /etc/rc.local << EOF
+# Configure ZRAM (512MB)
+modprobe zram
+echo lz4 > /sys/block/zram0/comp_algorithm
+echo 536870912 > /sys/block/zram0/disksize  # 512MB
+mkswap /dev/zram0
+swapon /dev/zram0
+EOF
+
+cat >> /etc/sysctl.conf << EOF
+net.core.rmem_max = 33554432
+net.core.wmem_max = 33554432
+net.core.rmem_default = 1048576
+net.core.wmem_default = 1048576
+net.core.optmem_max = 65536
+net.core.somaxconn = 131072
+net.ipv4.tcp_mem = 102400 204800 409600
+net.core.netdev_max_backlog = 131072
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_bbr_cca_params = max_bw_gain:10,min_rtt_gain:2
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1 
+net.ipv4.tcp_tw_recycle = 0  
+net.ipv4.tcp_max_tw_buckets = 2000000
+net.ipv4.tcp_max_syn_backlog = 65535
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_dsack = 1
+net.ipv4.tcp_fack = 1
+net.netfilter.nf_conntrack_max = 131072
+net.netfilter.nf_conntrack_tcp_timeout_established = 86400
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 30
+net.netfilter.nf_conntrack_udp_timeout = 30
+net.netfilter.nf_conntrack_udp_timeout_stream = 120
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+vm.dirty_ratio = 30
+vm.dirty_background_ratio = 10
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+vm.swappiness=10
+vm.vfs_cache_pressure=50
+net.ipv4.tcp_rmem=4096 131072 33554432
+net.ipv4.tcp_wmem=4096 131072 33554432
+
+EOF
